76c6f6993c9f75f0a6dd654dbc5885d7bdd59ede
RheaGums <dhwlsals99@gmail.com>
2026-07-31 17:25:49 +0900

    feat: 게임모드에서 랜덤숫자 생성, 입력 유효성 검사 구현
    feat: 게임모드에서 랜덤숫자 생성, 입력 유효성 검사 구현

--- Source/HW6/Private/HW6GameMode.cpp
+++ Source/HW6/Private/HW6GameMode.cpp
@@ -12,3 +12,88 @@ AHW6GameMode::AHW6GameMode()
 	PlayerControllerClass = AHW6PlayerController::StaticClass();
 	PlayerStateClass = AHW6PlayerState::StaticClass();
 }
+
+void AHW6GameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GenerateRandomNumbers();
+}
+
+void AHW6GameMode::GenerateRandomNumbers()
+{
+	if (!HasAuthority())
+	{
+		return;
+	}
+
+	SecretNumbers.Reset();
+
+	while (SecretNumbers.Num() < SecretNumberLength)
+	{
+		const int32 Candidate = FMath::RandRange(MinSecretDigit, MaxSecretDigit);
+
+		if (!SecretNumbers.Contains(Candidate))
+		{
+			SecretNumbers.Add(Candidate);
+		}
+	}
+
+	UE_LOG(
+		LogTemp,
+		Warning,
+		TEXT("[Server] Secret Number: %d%d%d"),
+		SecretNumbers[0],
+		SecretNumbers[1],
+		SecretNumbers[2]
+	);
+}
+
+bool AHW6GameMode::ValidateInput(
+	const FString& Input,
+	FString& OutValidatedInput,
+	FString& OutErrorMessage
+) const
+{
+	OutValidatedInput.Reset();
+	OutErrorMessage.Reset();
+
+	const FString NormalizedInput = Input.TrimStartAndEnd();
+
+	if (NormalizedInput.Len() != SecretNumberLength)
+	{
+		OutErrorMessage = TEXT("3자리 숫자를 입력해주세요.");
+		return false;
+	}
+
+	TArray<TCHAR> SeenDigits;
+	SeenDigits.Reserve(SecretNumberLength);
+
+	for (int32 Index = 0; Index < NormalizedInput.Len(); ++Index)
+	{
+		const TCHAR Digit = NormalizedInput[Index];
+
+		if (!FChar::IsDigit(Digit))
+		{
+			OutErrorMessage = TEXT("숫자만 입력해주세요.");
+			return false;
+		}
+
+		if (Digit == TEXT('0'))
+		{
+			OutErrorMessage = TEXT("1부터 9까지의 숫자를 입력해주세요.");
+			return false;
+		}
+
+		if (SeenDigits.Contains(Digit))
+		{
+			OutErrorMessage = TEXT("중복되지 않은 숫자를 입력해주세요.");
+			return false;
+		}
+
+		SeenDigits.Add(Digit);
+	}
+
+	OutValidatedInput = NormalizedInput;
+	return true;
+}
--- Source/HW6/Public/HW6GameMode.h
+++ Source/HW6/Public/HW6GameMode.h
@@ -13,4 +13,21 @@ class HW6_API AHW6GameMode : public AGameModeBase
 
 public:
 	AHW6GameMode();
+
+protected:
+	virtual void BeginPlay() override;
+
+	void GenerateRandomNumbers();
+	bool ValidateInput(
+		const FString& Input,
+		FString& OutValidatedInput,
+		FString& OutErrorMessage
+	) const;
+
+private:
+	static constexpr int32 SecretNumberLength = 3;
+	static constexpr int32 MinSecretDigit = 1;
+	static constexpr int32 MaxSecretDigit = 9;
+
+	TArray<int32> SecretNumbers;
 };
