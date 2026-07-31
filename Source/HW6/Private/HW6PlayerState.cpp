c2ae0669ab3d71e4d3d0448f700cff58f84c2c7a
RheaGums <dhwlsals99@gmail.com>
2026-07-31 16:59:35 +0900

    feat: PlayerState가 각 플레이어의 시도횟수 관리, 서버에서만 수정, 클라이언트는 복제
    feat: PlayerState가 각 플레이어의 시도횟수 관리, 서버에서만 수정, 클라이언트는 복제

--- Source/HW6/Private/HW6PlayerState.cpp
+++ Source/HW6/Private/HW6PlayerState.cpp
@@ -1,3 +1,76 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "HW6PlayerState.h"
+
+#include "Net/UnrealNetwork.h"
+
+AHW6PlayerState::AHW6PlayerState()
+	: CurrentAttempts(0)
+	, MaxAttempts(3)
+{
+}
+
+void AHW6PlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AHW6PlayerState, CurrentAttempts);
+}
+
+int32 AHW6PlayerState::GetCurrentAttempts() const
+{
+	return CurrentAttempts;
+}
+
+int32 AHW6PlayerState::GetMaxAttempts() const
+{
+	return MaxAttempts;
+}
+
+bool AHW6PlayerState::HasAttemptsLeft() const
+{
+	return CurrentAttempts < MaxAttempts;
+}
+
+bool AHW6PlayerState::AddAttempt()
+{
+	if (!HasAuthority() || !HasAttemptsLeft())
+	{
+		return false;
+	}
+
+	++CurrentAttempts;
+
+	UE_LOG(
+		LogTemp,
+		Log,
+		TEXT("[Server] %s used an attempt: %d/%d"),
+		*GetPlayerName(),
+		CurrentAttempts,
+		MaxAttempts
+	);
+
+	return true;
+}
+
+void AHW6PlayerState::ResetAttempts()
+{
+	if (!HasAuthority())
+	{
+		return;
+	}
+
+	CurrentAttempts = 0;
+}
+
+void AHW6PlayerState::OnRep_CurrentAttempts()
+{
+	UE_LOG(
+		LogTemp,
+		Log,
+		TEXT("[Client] %s received attempts: %d/%d"),
+		*GetPlayerName(),
+		CurrentAttempts,
+		MaxAttempts
+	);
+}
--- Source/HW6/Public/HW6PlayerState.h
+++ Source/HW6/Public/HW6PlayerState.h
@@ -10,4 +10,26 @@ UCLASS()
 class HW6_API AHW6PlayerState : public APlayerState
 {
 	GENERATED_BODY()
+
+public:
+	AHW6PlayerState();
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	int32 GetCurrentAttempts() const;
+	int32 GetMaxAttempts() const;
+	bool HasAttemptsLeft() const;
+
+	bool AddAttempt();
+	void ResetAttempts();
+
+protected:
+	UFUNCTION()
+	void OnRep_CurrentAttempts();
+
+	UPROPERTY(ReplicatedUsing = OnRep_CurrentAttempts, VisibleAnywhere, BlueprintReadOnly, Category = "Number Baseball")
+	int32 CurrentAttempts;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Number Baseball", meta = (ClampMin = "1"))
+	int32 MaxAttempts;
 };
